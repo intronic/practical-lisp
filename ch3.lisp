517d9b9436279003b5e7c1c76351531f889caac8
Michael Pheasant <mike@pheasant.co.nz>
2009-09-22 16:21:47 +1000

    up to macro

--- ch3.lisp
+++ ch3.lisp
@@ -7,6 +7,71 @@
 (defun add-record (cd) (push cd *db*))
 
 (defun dump-db ()
+  "Print the CD database"
   (dolist (cd *db*)
     (format t "~{~a:~10t~a~%~}~%" cd)))
+
+(defun dump-db2 ()
+    (format t "~{~{~a:~10t~a~%~}~%~}" *db*))
+
+;(add-record (make-cd "Fly" "DC" 7 t))
+;(add-record (make-cd "Roses" "DC" 8 t))
+
+(defun prompt-read (prompt)
+  (format *query-io* "~a: " prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
+
+(defun prompt-for-cd ()
+  (make-cd 
+   (prompt-read "Title")
+   (prompt-read "Artist")
+   (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+   (y-or-n-p "Ripped [y/n]: ")))
+
+(defun add-cds ()
+  (loop (add-record (prompt-for-cd))
+     (if (not (y-or-n-p "Another? [y/n]: ")) (return))))
+
+(defun save-db (filename)
+  (with-open-file (out filename
+		       :direction :output
+		       :if-exists :supersede)
+    (with-standard-io-syntax
+      (print *db* out))))
+
+(defun load-db (filename)
+  (with-open-file (in filename)
+    (with-standard-io-syntax 
+      (setf *db* (read in)))))
+
+(defun artist-selector (artist)
+  #'(lambda (cd) (equal (getf cd :artist) artist)))
+
+(defun select (selector-fn)
+  (remove-if-not selector-fn *db*))
+
+(defun where (&key title artist rating (ripped nil ripped-p))
+  #'(lambda (cd)
+      (and
+       (if title     (equal (getf cd :title) title) t)
+       (if artist    (equal (getf cd :artist) artist) t)
+       (if rating    (equal (getf cd :rating) rating) t)
+       (if ripped-p  (equal (getf cd :ripped) ripped) t))))
+
+(defun update (selector-fn &key title artist rating (ripped nil ripped-p))
+  (setf *db* (mapcar 
+	      #'(lambda (row)
+		  (when (funcall selector-fn row)
+		    (if title     (setf (getf row :title) title))
+		    (if artist    (setf (getf row :artist) artist))
+		    (if rating    (setf (getf row :rating) rating))
+		    (if ripped-p  (setf (getf row :ripped) ripped)))
+		  row) *db*)))
+
+(defun delete-rows (selector-fn)
+  (setf *db* (remove-if selector-fn *db*)))
+
+(defmacro backwards (expr)
+  (reverse expr))
 
